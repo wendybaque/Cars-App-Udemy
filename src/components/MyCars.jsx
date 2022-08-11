eafb696fc30141a34e3aff296037a3842f6ed4ce
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-08-11 12:08:13 +0200

    Vidéo 29

--- src/components/MyCars.jsx
+++ src/components/MyCars.jsx
@@ -6,10 +6,10 @@ import Wrapper from "./Wrapper";
 class Mycars extends Component {
     state = {
         cars : [
-            {name: 'Renault', color: "gray", year:2000},
-            {name: 'Peugeot', color: "blue", year:2001},
+            {name: 'Renault', color: "gray", year:2022},
+            {name: 'Peugeot', color: "blue", year:2021},
             {name: 'Toyota', color: "black", year:2002},
-            {name: 'Ferrari', color: "red", year:2003},
+            {name: 'Ferrari', color: "red", year:2001},
         ]
     }
 
@@ -39,8 +39,27 @@ class Mycars extends Component {
         })
     }
 
+    // Calcule l'âge de la voiture en fonction de l'année
+    getAge = (year) => {
+        const now = new Date().getFullYear();
+       const age =  now - year;
+
+       // Gestion de l'affichage de year.s au pluriel ou non en fonction du nombre d'années :
+       let yearStr = "";
+
+       if (age === 1) {
+        yearStr = "year";
+       } else if (age > 1) {
+        yearStr = "years";
+
+       }
+       return `${age} ${yearStr}`
+    }
+
     render() {
-        const year = new Date().getFullYear();
+        // const [Renault, Peugeot, Toyota, Ferrari] = this.state.cars;
+
+        // const year = new Date().getFullYear();
         return (
             <div className="cars">
                 <Wrapper>
@@ -50,12 +69,31 @@ class Mycars extends Component {
                     <p onCopy={this.noCopy} onMouseOver={this.addStyle}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi iste fugiat nisi doloribus iusto recusandae minima voluptate magni soluta? Placeat nostrum enim debitis distinctio in totam veritatis ea dolorum harum!</p>
                     <button onClick={this.addTenYears}>Cars - 10 year</button>
                 </Wrapper>
+
+                {/* Idem map cf plus bas mais avec tableau destructuré : */}
+                    {/* <Car
+                        name={Renault.name}
+                        color={Renault.color} 
+                        year={Renault.year}/>
+                    <Car 
+                        name={Peugeot.name} 
+                        color={Peugeot.color}
+                        year={Peugeot.year}/>
+                    <Car 
+                        name={Toyota.name}
+                        color={Toyota.color}
+                        year={Toyota.year}/>
+                    <Car 
+                        name={Ferrari.name}
+                        color={Ferrari.color}
+                        year={Ferrari.year}/> */}
+
                 {/* <Car color={this.state.cars[0].color} year={year - this.state.cars[0].year + ' year old'}>{this.state.cars[0].name}</Car>
                 <Car color={this.state.cars[1].color} year={year - this.state.cars[1].year + ' year old'}>{this.state.cars[1].name}</Car>
                 <Car color={this.state.cars[2].color} year={year - this.state.cars[2].year + ' year old'}>{this.state.cars[2].name}</Car>
                 <Car color={this.state.cars[3].color} year={year - this.state.cars[3].year + ' year old'}>{this.state.cars[3].name}</Car> */}
                 {/* Transformé avec une méthode map (attention à bien ajouter la key et l'index !) + on a changé children en name : */}
-                {
+                {/* {
                     this.state.cars.map((cars, index) => {
                         return(
                             <div  key={index}>
@@ -63,6 +101,24 @@ class Mycars extends Component {
                             </div>
                         )
                     })
+                } */}
+                {/* Idem mais avec fonction map destructurée : */}
+                {
+                    this.state.cars.map(({name, color, year}, index) => {
+                        return(
+                            <div  key={index}>
+                                <Car 
+                                name={name} 
+                                color={color} 
+                                year={this.getAge(year)}
+                                />
+                            </div>
+                        )
+                    })
+                }
+                {/* Fonction qui n'a rien à voir avec l'exercice, mais je la laisse là pour la retrouver */}
+                {
+                    Math.random()
                 }
             </div>
         )
