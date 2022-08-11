6ab337ffb30683b38487ff048f8155bc001d9f60
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-08-11 10:08:09 +0200

    Vidéo 25

--- src/App.js
+++ src/App.js
@@ -10,10 +10,48 @@ class App extends Component {
     title: 'My car catalog',
     color: 'blue'
 }
+
+// Change le titre quand on clique sur le bouton 
+changeTitle = (e) => {
+  console.log(e.target);
+  this.setState({
+    title:'My new car catalog'
+  })
+}
+
+// Change le titre via le state et le paramètre title
+changeViaParams = (title) => {
+  console.log(title);
+  this.setState({
+    title:title
+  })
+}
+
+// Change le titre via un bind
+changeViaBind = (param) => {
+  console.log(param);
+  this.setState({
+    title:param
+  })
+}
+
+// Change le titre via ce que l'utilisateur remplit dans l'input
+changeViaInput = (e) => {
+  console.log(e.target.value)
+  this.setState({
+    title:e.target.value
+  })
+}
+
   render() {
     return (
       <div className="App">
         <Mycars title={this.state.title} color={this.state.color} />
+        <button onClick={this.changeTitle}>Change title</button>
+        {/* Fonction fléchée pour ne pas que la fonction changeViaParams se lance directement au chargement de la page */}
+        <button onClick={() => this.changeViaParams('My car catalog via params')}>Change title via params</button>
+        <button onClick={this.changeViaBind.bind(this, 'My car catalog via bind')}>Change title via bind</button>
+        <input type="text" onChange={this.changeViaInput} value={this.state.title}></input>
       </div>
     );
   }
--- src/components/MyCars.jsx
+++ src/components/MyCars.jsx
@@ -7,6 +7,23 @@ class Mycars extends Component {
     state = {
         cars : ["Peugeot", "Renault", "Toyota"]
     }
+
+    // Ouvre un popup quand on tente de copier le texte
+    noCopy = () => {
+        alert("Merci de ne pas copier le texte !");
+    }
+
+    // Quand on passe la souris sur le texte, il se met en majuscules et violet
+    addStyle = (e) => {
+        console.log(e.target);
+        if(e.target.classList.contains('styled')) {
+            e.target.classList.remove('styled');
+
+        } else { 
+            e.target.classList.add('styled');
+        }
+    }
+
     render() {
 
         return (
@@ -15,6 +32,7 @@ class Mycars extends Component {
                     <MyHeader myStyle={this.props.color}>
                         {this.props.title}
                     </MyHeader>
+                    <p onCopy={this.noCopy} onMouseOver={this.addStyle}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi iste fugiat nisi doloribus iusto recusandae minima voluptate magni soluta? Placeat nostrum enim debitis distinctio in totam veritatis ea dolorum harum!</p>
                 </Wrapper>
                 <Car color="black">{this.state.cars[0]}</Car>
                 <Car>{this.state.cars[1]}</Car>
